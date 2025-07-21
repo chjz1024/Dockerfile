42fe8b42884a924707973daacd80c330a96644be
chjz1024 <ying57940815@gmail.com>
2025-07-21 17:02:28 +0800

    Update setup.sh

--- cuda/setup.sh
+++ cuda/setup.sh
@@ -11,7 +11,7 @@ sed -i 's/security.ubuntu.com/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
 
 apt update
 apt upgrade -y
-apt install -y --no-install-recommends curl wget git sudo neovim tmux less ssh
+apt install -y --no-install-recommends curl wget git sudo neovim tmux less ssh zsh zsh-syntax-highlighting zsh-autosuggestions
 rm -rf /var/lib/apt/lists/*
 
 curl -s -o miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-${MINICONDA_VERSION}-Linux-x86_64.sh
@@ -26,3 +26,7 @@ echo "${USERNAME} ALL=(ALL) NOPASSWD: ALL" >/etc/sudoers.d/${USERNAME}
 chsh -s /bin/bash ${USERNAME}
 chown -R ${USERNAME}:${USERNAME} ${MINICONDA_PATH}
 chown -R ${USERNAME}:${USERNAME} /home/${USERNAME}
+
+sudo -u user sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended &&
+    sudo -u user git clone --branch 24.09.04 https://github.com/marlonrichert/zsh-autocomplete.git /home/user/.oh-my-zsh/custom/plugins/zsh-autocomplete &&
+    sudo -u user mv /home/user/.zshrc.pre-oh-my-zsh /home/user/.zshrc
