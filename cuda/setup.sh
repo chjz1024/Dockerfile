4cd6047efc3a792b7d2a4c9b7a4a62506cf8bb19
chjz1024 <ying57940815@gmail.com>
2025-07-21 17:15:53 +0800

    Update setup.sh

--- cuda/setup.sh
+++ cuda/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 export DEBIAN_FRONTEND=noninteractive
 export MINICONDA_VERSION=latest
 export MINICONDA_PATH=/opt/miniconda
@@ -9,9 +11,9 @@ export USER_UID=1000
 sed -i 's@//.*archive.ubuntu.com@//mirrors.ustc.edu.cn@g' /etc/apt/sources.list
 sed -i 's/security.ubuntu.com/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
 
-apt update
-apt upgrade -y
-apt install -y --no-install-recommends curl wget git sudo neovim tmux less ssh zsh zsh-syntax-highlighting zsh-autosuggestions
+apt-get update
+apt-get upgrade -y
+apt-get install -y --no-install-recommends curl wget git sudo neovim tmux less ssh zsh zsh-syntax-highlighting zsh-autosuggestions
 rm -rf /var/lib/apt/lists/*
 
 curl -s -o miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-${MINICONDA_VERSION}-Linux-x86_64.sh
@@ -23,10 +25,12 @@ useradd -m ${USERNAME} --uid=${USER_UID}
 usermod -aG sudo ${USERNAME}
 echo 'user:password' | chpasswd
 echo "${USERNAME} ALL=(ALL) NOPASSWD: ALL" >/etc/sudoers.d/${USERNAME}
-chsh -s /bin/bash ${USERNAME}
+chsh -s /bin/zsh ${USERNAME}
 chown -R ${USERNAME}:${USERNAME} ${MINICONDA_PATH}
 chown -R ${USERNAME}:${USERNAME} /home/${USERNAME}
 
 sudo -u user sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended &&
     sudo -u user git clone --branch 24.09.04 https://github.com/marlonrichert/zsh-autocomplete.git /home/user/.oh-my-zsh/custom/plugins/zsh-autocomplete &&
+    sudo -u user git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /home/user/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting &&
+    sudo -u user git clone https://github.com/zsh-users/zsh-autosuggestions.git /home/user/.oh-my-zsh/custom/plugins/zsh-autosuggestions &&
     sudo -u user mv /home/user/.zshrc.pre-oh-my-zsh /home/user/.zshrc
